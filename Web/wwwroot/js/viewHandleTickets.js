43c5cfaee2f0713d8de24e5141b85b4be0830caa
Fahmi Anwar <fahmi@saungawi.com>
2021-07-02 17:11:08 +0700

    Update Convertation in ViewHandleTickets

--- Web/wwwroot/js/viewHandleTickets.js
+++ Web/wwwroot/js/viewHandleTickets.js
@@ -40,10 +40,10 @@
             {
                 "render": function (data, type, row) {
                     if (row['endDateTime'] == null) {
-                        if (row['level'] == roleId) {
+                        if (row['level'] == levelRole) {
                             return `<button type="button" class="btn btn-info" onclick="askNextLevel('${row['id']}')">Ask Next Level</button> | <button type="button" class="btn btn-primary" onclick="viewConvertation('${row['id']}')" data-toggle="modal" data-target="#viewConvertationModal">Chat</button> | <button type="button" class="btn btn-danger" onclick="closeTicket('${row['id']}','${userId}')">Close</button>`;
                         } else {
-                            return null;
+                            return levelRole;
                         }
                         //return `<button type="button" class="btn btn-info" onclick="askNextLevel('${row['id']}')">Ask Next Level</button> | <button type="button" class="btn btn-primary" onclick="viewConvertation('${row['id']}')" data-toggle="modal" data-target="#viewConvertationModal">Chat</button> | <button type="button" class="btn btn-danger" onclick="closeTicket('${row['id']}','${userId}')">Close</button>`;
                     } else {
@@ -59,6 +59,32 @@
 
 function viewConvertation(caseId) {
     $("#inputConvertationCaseId").val(parseInt(caseId));
+    text = "";
+    text += `
+                    <div class="direct-chat-msg">
+                        <div class="direct-chat-infos clearfix">
+                            <span class="direct-chat-name float-left">Alexander Pierce</span>
+                            <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
+                        </div>
+                        <img class="direct-chat-img" src="~/lib/adminlte/img/user1-128x128.jpg" alt="message user image">
+                        <div class="direct-chat-text">
+                            Is this template really for free? That's unbelievable!
+                        </div>
+                    </div>
+            `;
+    text += `
+                    <div class="direct-chat-msg right">
+                        <div class="direct-chat-infos clearfix">
+                            <span class="direct-chat-name float-right">Sarah Bullock</span>
+                            <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
+                        </div>
+                        <img class="direct-chat-img" src="~/lib/adminlte/img/user3-128x128.jpg" alt="message user image">
+                        <div class="direct-chat-text">
+                            You better believe it!
+                        </div>
+                    </div>
+            `;
+    $("#chatMessages").html(text);
 }
 
 function createConvertation() {
