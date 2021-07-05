1ba8b98065fc764642ab80c8aed6d9ba8581971a
sitinabilarahmah <sitinabilarahmah@gmail.com>
2021-07-05 22:23:37 +0700

    GetConvertation

--- API/ViewModels/ConvertationVM.cs
+++ API/ViewModels/ConvertationVM.cs
@@ -14,5 +14,6 @@ namespace API.ViewModels
         public int CaseId { get; set; }
         public int UserId { get; set; }
         public string UserName { get; set; }
+        public string Avatar { get; set; }
     }
 }
