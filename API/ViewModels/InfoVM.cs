ccb2de8f1fb112c38efe981685b1ec3abab64001
Fahmi Anwar <fahmi@saungawi.com>
2021-07-09 10:29:56 +0700

    Update InfoVM typo

--- API/ViewModels/InfoVM.cs
+++ API/ViewModels/InfoVM.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace API.ViewModels
 {
-    public class LoginVM
+    public class InfoVM
     {
         public string Email { get; set; }
         public string Password { get; set; }
